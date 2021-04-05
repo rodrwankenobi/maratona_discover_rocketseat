c9a056f608cb534ef8f3dc0a7281ff78bb0ec087
rodrwankenobi <49798159+rodrwankenobi@users.noreply.github.com>
2021-04-05 08:27:35 -0300

    Aula 2

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+// utilizando a template engine
 server.set("view engine","ejs")
+
+// utilizando o req.body
+server.use(express.urlencoded({ extended: true }))
 
 // arquivos estaticos
 server.use(express.static("public"))
